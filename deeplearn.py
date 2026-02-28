32d782059b19bbd0a0aacbe2385c309c61cb32a9
meganliao021-commits <meganliao021@gmail.com>
2026-02-28 16:56:56 +0800

    Update deeplearn.py

--- deeplearn.py
+++ deeplearn.py
@@ -121,3 +121,29 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+from transformers import pipeline
+from PIL import Image
+import streamlit as st
+
+# Streamlit UI
+print("Title: Age Classification using ViT")
+
+# Load the age classification pipeline
+# The code below should be placed in the main part of the program
+age_classifier = pipeline("image-classification",
+                          model="nateraw/vit-age-classifier")
+
+image_name = "middleagedWoman.jpg"
+image_name = Image.open(image_name).convert("RGB")
+
+# Classify age
+age_predictions = age_classifier(image_name)
+print(age_predictions)
+age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+
+# Display results
+print("Predicted Age Range:")
+print(f"Age range: {age_predictions[0]['label']}")
