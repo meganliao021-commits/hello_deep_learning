bd6cc9c995b6aaf3f9bb5bdd98644e1c796db624
meganliao021-commits <meganliao021@gmail.com>
2026-03-03 23:43:04 +0800

    Update 21198707_asg.py

--- 21198707_asg.py
+++ 21198707_asg.py
@@ -30,7 +30,7 @@ def text2story(scenario):
     Model: aspis/gpt2-genre-story-generation.
     """
     # Initialize text generation pipeline
-    story_gen = pipeline("text-generation", model="DanielG9/Story-LLM")
+    story_gen = pipeline("text-generation", model="microsoft/phi-1_5")
     
     # Crafting a prompt to guide GPT-2 for children's storytelling
     # Setting the genre helps maintain the tone for kids
