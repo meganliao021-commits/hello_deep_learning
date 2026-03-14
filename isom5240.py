121d219f10cc69ee5d5b7a46e01fdbeccec4bd9f
meganliao021-commits <meganliao021@gmail.com>
2026-03-14 15:34:52 +0800

    Update isom5240.py

--- isom5240.py
+++ isom5240.py
@@ -1,1 +1,21 @@
 
+import streamlit as st
+from transformers import pipeline
+
+def main():
+    sentiment_pipeline = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+    st.title("Sentiment Analysis with HuggingFace Spaces")
+    st.write("Enter a sentence to analyze its sentiment:")
+
+    user_input = st.text_input("")
+    if user_input:
+        result = sentiment_pipeline(user_input)
+        sentiment = result[0]["label"]
+        confidence = result[0]["score"]
+
+        st.write(f"Sentiment: {sentiment}")
+        st.write(f"Confidence: {confidence:.2f}")
+
+if __name__ == "__main__":
+    main()
